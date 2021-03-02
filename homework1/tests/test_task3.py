98324f6f3f0eec570adf4e1b8fcfcd9ecbb2d94a
Artem Davletov <Tomas-Cooper@mail.ru>
2021-03-02 19:32:06 +0300

    Add random numbers generation in test for task3

--- homework1/tests/test_task3.py
+++ homework1/tests/test_task3.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from random import randint
 
 import pytest
 
@@ -6,12 +6,38 @@ from homework1.task03 import find_maximum_and_minimum
 
 
 @pytest.mark.parametrize(
-    ["file_name", "expected_result"],
+    ["radius", "number"],
     [
-        ("tests/test_data/task3_test.txt", (-1000, 1000))
+        (1000, 100),
+        (10000, 1000)
     ],
 )
-def test_find_maximum_and_minimum(file_name: str, expected_result: Tuple[int, int]):
+def test_find_maximum_and_minimum(radius: int,
+                                  number: int,
+                                  file_name: str = "tests/test_data/task3_test.txt"):
+    min_val = 0
+    max_val = 0
+
+    i = 1
+    with open(file_name, "r+") as file:
+        values = []
+        while i < number:
+            val = randint(-1 * radius, radius)
+
+            if val < min_val:
+                min_val = val
+
+            if val > max_val:
+                max_val = val
+
+            values.append(str(val))
+
+            if i % 10 == 0:
+                file.write(" ".join(values) + "\n")
+                values = []
+
+            i += 1
+
     assert (
-        find_maximum_and_minimum(file_name=file_name) == expected_result
+            find_maximum_and_minimum(file_name=file_name) == (min_val, max_val)
     ), f"Incorrect answer for file: {file_name}"
