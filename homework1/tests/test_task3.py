3e7e38b47e35e521b6bb70398b834abf13dbcda3
Artem Davletov <Tomas-Cooper@mail.ru>
2021-03-03 11:51:50 +0300

    Use formatters

--- homework1/tests/test_task3.py
+++ homework1/tests/test_task3.py
@@ -7,14 +7,11 @@ from homework1.task03 import find_maximum_and_minimum
 
 @pytest.mark.parametrize(
     ["radius", "number"],
-    [
-        (1000, 100),
-        (10000, 1000)
-    ],
+    [(1000, 100), (10000, 1000)],
 )
-def test_find_maximum_and_minimum(radius: int,
-                                  number: int,
-                                  file_name: str = "tests/test_data/task3_test.txt"):
+def test_find_maximum_and_minimum(
+    radius: int, number: int, file_name: str = "tests/test_data/task3_test.txt"
+):
     min_val = 0
     max_val = 0
 
@@ -39,6 +36,7 @@ def test_find_maximum_and_minimum(radius: int,
 
             i += 1
 
-    assert (
-            find_maximum_and_minimum(file_name=file_name) == (min_val, max_val)
+    assert find_maximum_and_minimum(file_name=file_name) == (
+        min_val,
+        max_val,
     ), f"Incorrect answer for file: {file_name}"
