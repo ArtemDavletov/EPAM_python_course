d66c65ea93323712d2f6bae0e32f99905bba4676
Artem Davletov <Tomas-Cooper@mail.ru>
2021-03-09 13:41:40 +0300

    Add solutions for third homework

---
+++ homework3/task01/task01.py
@@ -0,0 +1,32 @@
+from typing import Callable
+
+
+def cache(times: int):
+    cacher = dict()
+
+    def decorator(func: Callable) -> Callable:
+        def wrapper(*args, **kwargs):
+            if func.__name__ not in cacher:
+                cacher[func.__name__] = [func(), times]
+            else:
+                if cacher[func.__name__][1] == 0:
+                    del cacher[func.__name__]
+                    cacher[func.__name__] = [func(), times]
+                cacher[func.__name__][1] -= 1
+
+            return cacher[func.__name__][0]
+
+        return wrapper
+
+    return decorator
+
+
+@cache(times=2)
+def f():
+    return input("? ")
+
+
+print(f())
+print(f())
+print(f())
+print(f())
