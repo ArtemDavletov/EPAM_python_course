a947d3233dd62d6bfb0fd20d6e186682d73b6686
Artem Davletov <Tomas-Cooper@mail.ru>
2021-03-16 17:28:02 +0300

    Add tests for second task

--- homework4/tests/task2_test.py
+++ homework4/tests/task2_test.py
@@ -1,19 +1,35 @@
-# from unittest.mock import Mock
-#
-# import pytest
-# from requests import Response
-#
-# from homework4.task_2_mock_input import count_dots_on_i
-#
-# @pytest.mark.trylast
-# def test_positive_count_dots_on_i():
-#     mock = Mock()
-#
-#     count_dots_on_i(mock)
-#
-#
-# @pytest.mark.trylast
-# def test_negative_count_dots_on_i():
-#     response = Response()
-#     url = Mock(response)
-#     count_dots_on_i(str())
+from collections import Counter
+from unittest.mock import MagicMock
+
+import pytest
+
+
+class ResponseMock:
+    text: str
+
+    def __init__(self, text: str):
+        self.text = text
+
+
+class RequestsMock:
+    ...
+
+
+def count_dots_on_i_mock(url: str) -> int:
+    try:
+        requests = RequestsMock()
+        requests.get = MagicMock(return_value=ResponseMock(text=url))
+        return Counter(requests.get(url).text)["i"]
+    except Exception:
+        raise ValueError(f"Unreachable {url}")
+
+
+@pytest.mark.parametrize(["text", "expected_value"], [("<i></i>", 2), ("", 0), ("i", 1), ("iiiii", 5)])
+def test_positive_count_dots_on_i(text, expected_value: int):
+    assert count_dots_on_i_mock(text) == expected_value
+
+
+@pytest.mark.parametrize("text", [123, ResponseMock])
+def test_negative_count_dots_on_i(text):
+    with pytest.raises(ValueError):
+        count_dots_on_i_mock(text)
