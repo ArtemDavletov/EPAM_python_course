52ffb44a27bbbae94d7a5266d39ed6347abc8bb9
Artem Davletov <Tomas-Cooper@mail.ru>
2021-04-27 18:04:29 +0300

    Add homework 12

---
+++ homework12/alembic/versions/cba9e1f1b7d5_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: cba9e1f1b7d5
+Revises: 4e609a88900f
+Create Date: 2021-04-27 17:48:14.917736
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "cba9e1f1b7d5"
+down_revision = "4e609a88900f"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
