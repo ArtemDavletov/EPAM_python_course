0424d6ec3b3495451f6f70cb9e61778cd1e7871f
Artem Davletov <Tomas-Cooper@mail.ru>
2021-05-11 16:22:12 +0300

    Change MagicMock to Mock in tests

--- homework11/tests/hw2_test.py
+++ homework11/tests/hw2_test.py
@@ -1,19 +1,19 @@
-from unittest.mock import MagicMock
+from unittest.mock import Mock
 
 from homework11.hw2 import Order
 
 
 def test_final_price_once():
-    morning_discount = MagicMock(return_value=0)
+    morning_discount = Mock(return_value=0)
     order_1 = Order(100, morning_discount)
     assert order_1.final_price() == 100
 
 
 def test_final_price_twice():
-    morning_discount = MagicMock(return_value=50)
+    morning_discount = Mock(return_value=50)
     order_1 = Order(100, morning_discount)
     assert order_1.final_price() == 50
 
-    elder_discount = MagicMock(return_value=90)
+    elder_discount = Mock(return_value=90)
     order_2 = Order(100, elder_discount)
     assert order_2.final_price() == 10
